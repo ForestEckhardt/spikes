17dc7cd84f74e40a66103444dcfa2c77924e6463
Forest Eckhardt <feckhardt@pivotal.io>
2021-09-30 20:14:28 +0000

    Adds input flags and ref-doc integration as well as working on markdown formatting

--- auto-doc/main.go
+++ auto-doc/main.go
@@ -1,42 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/pelletier/go-toml"
 )
 
+type BuildpackTOML struct {
+	Buildpack struct {
+		Name string `toml:"name"`
+	} `toml:"buildpack"`
+	Metadata struct {
+		Configurations []struct {
+			Name        string `toml:"name"`
+			Description string `toml:"description"`
+			Default     string `toml:"default"`
+			Build       bool   `toml:"build"`
+			Launch      bool   `toml:"launch"`
+		} `toml:"configurations"`
+	} `toml:"metadata"`
+}
+
 func main() {
-	file, err := os.Open("../ForestEckhardt/bellsoft-liberica/buildpack.toml")
+	var buildpackDir string
+	flag.StringVar(&buildpackDir, "buildpack-dir", "", "buildpack directory")
+	flag.Parse()
+
+	if buildpackDir == "" {
+		log.Fatal("required --buildpack-dir to be set")
+	}
+
+	file, err := os.Open(filepath.Join(buildpackDir, "buildpack.toml"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var envVars struct {
-		Metadata struct {
-			Configurations []struct {
-				Name        string `toml:"name"`
-				Description string `toml:"description"`
-				Default     string `toml:"default"`
-				Build       bool   `toml:"build"`
-				Launch      bool   `toml:"launch"`
-			} `toml:"configurations"`
-		} `toml:"metadata"`
-	}
+	var bpTOML BuildpackTOML
 
-	err = toml.NewDecoder(file).Decode(&envVars)
+	err = toml.NewDecoder(file).Decode(&bpTOML)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, e := range envVars.Metadata.Configurations {
-		str := fmt.Sprintf("%s\n%s\n", e.Name, e.Description)
+	output := fmt.Sprintf("# %s\n\n## Environment Variable Configuration\n", bpTOML.Buildpack.Name)
+
+	for _, e := range bpTOML.Metadata.Configurations {
+		str := fmt.Sprintf("### %s\n%s\n", e.Name, e.Description)
 
 		if e.Default != "" {
-			str = fmt.Sprintf("%sDefault Value:%s\n", str, e.Default)
+			str = fmt.Sprintf("%sDefault Value: `%s`\n", str, e.Default)
 		}
 
 		if e.Build {
@@ -47,6 +64,17 @@ func main() {
 			str = fmt.Sprintf("%sThis environment variable is used during launch\n", str)
 		}
 
-		fmt.Println(str)
+		output += str + "\n"
 	}
+
+	output += "## Additional Reference Information\n"
+
+	ref, err := os.ReadFile(filepath.Join(buildpackDir, "reference-doc.md"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output += string(ref)
+
+	fmt.Println(output)
 }
